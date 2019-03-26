89a48c6564434462a387f35af52592b11e0b50f5
Dustin Herboldshimer and Lindsey Baker <dustin@>
2019-03-26 11:21:17 -0700

    Add dark and light theme clickable buttons

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function(){
+  $("#darkButton").click(function(){
+    $("body").addClass("darkTheme");
+  });
+  $("#lightButton").click(function(){
+    $("body").removeClass("darkTheme");
+  });
+});
